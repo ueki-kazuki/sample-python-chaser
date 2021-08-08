f95701a74587c03b433608590ef64e9fba4a2e1a
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2021-08-08 11:12:12 +0900

    Rename muti-bytes variable names to single-byte

--- sample_blocker_bug.py
+++ sample_blocker_bug.py
@@ -1,17 +1,18 @@
-import CHaser # 同じディレクトリに CHaser.py がある前提
+import CHaser  # 同じディレクトリに CHaser.py がある前提
 
-上 = 1
-左 = 3
-右 = 5
-下 = 7
+UP = 1
+LEFT = 3
+RIGHT = 5
+DOWN = 7
 
-敵 = 1
-壁 = 2
-宝 = 3
+ENEMY = 1
+BLOCK = 2
+ITEM = 3
+
 
 def main():
-    value = [] # フィールド情報を保存するリスト
-    client = CHaser.Client() # サーバーと通信するためのインスタンス
+    value = []  # フィールド情報を保存するリスト
+    client = CHaser.Client()  # サーバーと通信するためのインスタンス
 
     # 前回位置を保存する変数
     zenkai = ""
@@ -20,28 +21,29 @@ def main():
         # 前回いた位置にブロックを置く
         if zenkai:
             value = client.get_ready()
-            if zenkai == 上:
+            if zenkai == UP:
                 value = client.put_down()
-            elif zenkai == 左:
+            elif zenkai == LEFT:
                 value = client.put_left()
-            elif zenkai == 下:
+            elif zenkai == DOWN:
                 value = client.put_down()
-            elif zenkai == 右:
+            elif zenkai == RIGHT:
                 value = client.put_left()
 
         # 壁がなければそこに移動する
         # 下右上左の順に壁のありなしをしらべる
         value = client.get_ready()
-        if value[下] != 壁:
+        if value[DOWN] != BLOCK:
             value = client.walk_down()
-            zenkai = 下
-        elif value[右] != 壁:
+            zenkai = DOWN
+        elif value[RIGHT] != BLOCK:
             value = client.walk_right()
-            zenkai = 右
-        elif value[上] != 壁:
+            zenkai = RIGHT
+        elif value[UP] != BLOCK:
             value = client.walk_up()
         else:
             value = client.walk_left()
+
 
 """
 python sample.py のようにこのファイルを直接実行すると，
